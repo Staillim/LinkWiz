bb5dfabed835950574b5c368785efc831e813014
GAMER STONE <harrigta@gmail.com>
2025-09-13 18:58:27 +0000

    falta tipo de Mobile, Android o iphone

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,13 +7,22 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getDeviceFromUserAgent(ua: string): string {
   if (!ua) return "Unknown";
-  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+
+  if (/iphone/i.test(ua)) return "iPhone";
+  if (/ipad/i.test(ua)) return "iPad";
+  if (/android/i.test(ua)) {
+    if (/mobile/i.test(ua)) {
+      return "Android";
+    }
     return "Tablet";
   }
-  if (/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+  if (/(tablet|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "Tablet";
+  }
+  if (/Mobile|iP(od)|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
     return "Mobile";
   }
-  if (/(windows phone)/i.test(ua)) return "Mobile";
+  if (/(windows phone)/i.test(ua)) return "Windows Phone";
   if (/(macintosh|mac os x)/i.test(ua)) return "Mac";
   if (/(windows|win32)/i.test(ua)) return "Windows";
   if (/(linux)/i.test(ua)) return "Linux";
